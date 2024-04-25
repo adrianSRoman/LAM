74d3c7e9b58427c7f421c18e164ec334d33bce8f
asroman <asroman@ucdavis.edu>
2024-04-24 23:14:35 -0700

    add optimized bproj model

--- model/bproj.py
+++ model/bproj.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 from torch import nn
+import math
 
 device = 'cuda:0'
 
@@ -17,7 +18,6 @@ class BackProjLayer(torch.nn.Module):
         super().__init__()
         self.A = torch.from_numpy(np.load("/scratch/data/repos/LAM/util/steering.npy")).to(device)
         self.A.requires_grad = False
-        self.Nch = Nch
         if tau is None or D is None:
             self.tau = torch.nn.Parameter(torch.empty((Npx), dtype=torch.float64))
             self.D = torch.nn.Parameter(torch.empty((Nch, Npx), dtype=torch.complex128))
@@ -25,31 +25,31 @@ class BackProjLayer(torch.nn.Module):
         else:
             self.tau = torch.nn.Parameter(tau)
             self.D = torch.nn.Parameter(D)
+            
 
     def reset_parameters(self):
         std = 1e-5
         self.tau.data.normal_(0, std)
         self.D.data.normal_(0, std)
 
-    def forward(self, S):
-        """Forward Pass.
-        Args:
-            S (:obj:`torch.Tensor`): input to be forwarded. (N_sample, Npx)
-        Returns:
-            :obj:`torch.Tensor`: output: (N_sample, Npx)
-        """
-        N_sample, N_px = S.shape[0], self.tau.shape[0]
-        latent_x = torch.zeros((N_sample, N_px)).to(device)
-        out = torch.zeros((N_sample, self.Nch, self.Nch), dtype=torch.complex128).to(device)
-        S = S.squeeze(1)
-        for i in range(N_sample): # Loop to handle linalg.eigh: broadcasting can be slower
-            Ds, Vs = torch.linalg.eigh(S[i]) # (Nch, Nch), (Nch, Nch)
-            idx = Ds > 0  # To avoid np.sqrt() issues.
-            Ds, Vs = Ds[idx], Vs[:, idx]
-            latent_x[i] = torch.linalg.norm(self.D.conj().T @ (Vs * torch.sqrt(Ds)), axis=1) ** 2 # (Npx, Nch) dot ((Nch, Nch) * (Nch, Nch))
-            latent_x[i] -= self.tau
-            #print("shape of out", (self.A @ torch.diag(latent_x[i].cdouble()) @ self.A.T.conj()).dtype)
-            out[i] = self.A @ torch.diag(latent_x[i].cdouble()) @ self.A.T.conj()
-        #print(out.dtype, latent_x.dtype)
-        return out, latent_x
 
+    def forward(self, S):
+        """Vectorized Forward Pass.
+        Args:
+            S (:obj:`torch.Tensor`): input to be forwarded. (batch_size, Nch, Nch)
+        Returns:
+            :obj:`torch.Tensor`: output: (batch_size, N_px)
+        """
+        S = S.squeeze(1)
+        batch_size, N_ch = S.shape[:2]
+        N_px = self.tau.shape[0]
+        Ds, Vs = torch.linalg.eigh(S)  # (batch_size, N_ch, N_ch), (batch_size, N_ch, N_ch)
+        idx = Ds > 0  # To avoid np.sqrt() issues.
+        Ds = torch.where(idx, Ds, torch.zeros_like(Ds)) # apply mask to Ds
+        Vs = Vs * torch.sqrt(Ds).unsqueeze(1) # element-wise multiplication between Vs and sqrt(Ds)
+        latent_x = torch.matmul(self.D.conj().T, Vs)
+        latent_x = torch.linalg.norm(latent_x, dim=2) ** 2 # norm operation along the second dimension and square the result
+        latent_x -= self.tau
+        expanded_A = self.A.unsqueeze(0) # expand to unit in batch dimension
+        out = torch.einsum('nij,bjk,nkl->bil', expanded_A, torch.diag_embed(latent_x.cdouble()), expanded_A.transpose(1, 2).conj())
+        return out, latent_x
