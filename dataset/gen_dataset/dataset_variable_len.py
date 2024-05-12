b063e7992b67234062516b7bf6b11a2d1f56dcb1
asroman <asroman@ucdavis.edu>
2024-05-11 23:46:02 -0700

    generate variable length sound events

---
+++ dataset/gen_dataset/dataset_variable_len.py
@@ -0,0 +1,232 @@
+import os
+from tqdm import tqdm
+import pickle
+import random
+from random import randrange
+import gc
+from glob import glob
+import h5py
+import numpy as np
+import scipy
+from scipy.io import wavfile
+
+from apgd import *
+
+import scipy.constants as constants
+import scipy.io.wavfile as wavfile
+import scipy.signal.windows as windows
+import scipy.linalg as linalg
+import skimage.util as skutil
+import scipy.sparse as sp
+import scipy.sparse.linalg as splinalg
+import scipy.spatial as spatial
+
+import librosa
+
+
+def extract_visibilities(_data, _rate, T, fc, bw, alpha):
+    """
+    Transform time-series to visibility matrices.
+
+    Parameters
+    ----------
+    T : float
+        Integration time [s].
+    fc : float
+        Center frequency [Hz] around which visibility matrices are formed.
+    bw : float
+        Double-wide bandwidth [Hz] of the visibility matrix.
+    alpha : float
+        Shape parameter of the Tukey window, representing the fraction of
+        the window inside the cosine tapered region. If zero, the Tukey
+        window is equivalent to a rectangular window. If one, the Tukey
+        window is equivalent to a Hann window.
+
+    Returns
+    -------
+    S : :py:class:`~numpy.ndarray`
+        (N_slot, N_channel, N_channel) visibility matrices (complex-valued).
+    """
+    N_stft_sample = int(_rate * T)
+    if N_stft_sample == 0:
+        raise ValueError('Not enough samples per time frame.')
+    # print(f'Samples per STFT: {N_stft_sample}')
+
+    N_sample = (_data.shape[0] // N_stft_sample) * N_stft_sample
+    N_channel = _data.shape[1]
+    stf_data = (skutil.view_as_blocks(_data[:N_sample], (N_stft_sample, N_channel))
+                .squeeze(axis=1))  # (N_stf, N_stft_sample, N_channel)
+
+    window = windows.tukey(M=N_stft_sample, alpha=alpha, sym=True).reshape(1, -1, 1)
+    stf_win_data = stf_data * window  # (N_stf, N_stft_sample, N_channel)
+    N_stf = stf_win_data.shape[0]
+
+    stft_data = np.fft.fft(stf_win_data, axis=1)  # (N_stf, N_stft_sample, N_channel)
+    # Find frequency channels to average together.
+    idx_start = int((fc - 0.5 * bw) * N_stft_sample / _rate)
+    idx_end = int((fc + 0.5 * bw) * N_stft_sample / _rate)
+    collapsed_spectrum = np.sum(stft_data[:, idx_start:idx_end + 1, :], axis=1)
+
+    # Don't understand yet why conj() on first term?
+    # collapsed_spectrum = collapsed_spectrum[0,:]
+    S = (collapsed_spectrum.reshape(N_stf, -1, 1).conj() *
+        collapsed_spectrum.reshape(N_stf, 1, -1))
+    return S
+
+def form_visibility(data, rate, fc, bw, T_sti, T_stationarity):
+    '''
+    Parameter
+    ---------
+    data : :py:class:`~numpy.ndarray`
+        (N_sample, N_channel) antenna samples. (float)
+    rate : int
+        Sample rate [Hz]
+    fc : float
+        Center frequency [Hz] around which visibility matrices are formed.
+    bw : float
+        Double-wide bandwidth [Hz] of the visibility matrix.
+    T_sti : float
+        Integration time [s]. (time-series)
+    T_stationarity : float
+        Integration time [s]. (visibility)
+        
+    Returns
+    -------
+    S : :py:class:`~numpy.ndarray`
+        (N_slot, N_channel, N_channel) visibility matrices.
+        
+        # N_slot == number of audio frames in track
+
+    Note
+    ----
+    Visibilities computed directly in the frequency domain.
+    For some reason visibilities are computed correctly using
+    `x.reshape(-1, 1).conj() @ x.reshape(1, -1)` and not the converse.
+    Don't know why at the moment.
+    '''
+    S_sti = (extract_visibilities(data, rate, T_sti, fc, bw, alpha=1.0))
+
+    N_sample, N_channel = data.shape
+    N_sti_per_stationary_block = int(T_stationarity / T_sti)
+    S = (skutil.view_as_windows(S_sti,
+                                (N_sti_per_stationary_block, N_channel, N_channel),
+                                (N_sti_per_stationary_block, N_channel, N_channel))
+        .squeeze(axis=(1, 2))
+        .sum(axis=1))
+    return S
+
+def get_visibility_matrix(audio_in, fs, apgd=False, bands=[3], T_sti=10e-3):
+    # audio_in, fs = self._load_audio(audio_filename)
+    nbands = 10
+    freq, bw = (skutil  # Center frequencies to form images
+        .view_as_windows(np.linspace(1500, 4500, nbands), (2,), 1)
+        .mean(axis=-1)), 50.0  # [Hz]
+
+    # freq, bw = librosa.mel_frequencies(n_mels=nbands-1, fmin=50, fmax=4500), 50
+    A = np.load("/scratch/data/repos/LAM/util/steering.npy")
+    print("steering matrix shape", A.shape)
+    N_px = A.shape[1]
+    print("shape of pixes", N_px)
+    visibilities = []
+    apgd_map = []
+    for i in range(nbands-1):
+        if i not in bands:
+            continue
+        T_sti = T_sti
+        T_stationarity = 10 * T_sti  # Choose to have frame_rate = 10
+        S = form_visibility(audio_in, fs, freq[i], bw, T_sti, T_stationarity)
+        N_sample = S.shape[0]
+        visibilities_per_frame = []
+        apgd_gamma = 0.5
+        apgd_per_band = np.zeros((N_sample, N_px))
+        I_prev = np.zeros((N_px,))
+        for s_idx in range(N_sample):
+            S_D, S_V = linalg.eigh(S[s_idx])
+            if S_D.max() <= 0:
+                S_D[:] = 0
+            else:
+                S_D = np.clip(S_D / S_D.max(), 0, None)
+            S_norm = (S_V * S_D) @ S_V.conj().T
+            visibilities_per_frame.append(S_norm)
+            if apgd:
+                I_apgd = solve(S_norm, A, gamma=apgd_gamma, x0=I_prev.copy(), verbosity='NONE')
+                apgd_per_band[s_idx] = I_apgd['sol']
+            else:
+                apgd_per_band[s_idx] = [0]
+        apgd_map.append(apgd_per_band)
+        visibilities.append(visibilities_per_frame)
+
+    return np.array(visibilities), np.array(apgd_map) 
+
+def create_full_hdf_data(dataset_name='train', data_src=None, save_path=None):
+    if not data_src or not save_path:
+        print("[Error] Provide a valid dataset source path or a valid path to save the generated dataset")
+        return
+    
+    eigenmike_files = []
+    if "train" in dataset_name:
+        eigenmike_files = [os.path.join(data_src, file) for file in os.listdir(data_src) if file.endswith('.wav') and "._" not in file and "fold1" in file]
+    elif "test" in dataset_name:
+        eigenmike_files = [os.path.join(data_src, file) for file in os.listdir(data_src) if file.endswith('.wav') and "._" not in file and "fold2" in file]
+    else:
+        eigenmike_files = [os.path.join(data_src, file) for file in os.listdir(data_src) if file.endswith('.wav') and "._" not in file]
+
+    T_logs_spaced = np.logspace(np.log10(20e-3), np.log10(200e-3), 9) # logarithmically space duration from 10ms to 2sec
+    T_logs_spaced = np.round(T_logs_spaced, 3) # round to three decimals
+
+    with h5py.File(os.path.join(save_path, f"{dataset_name}.hdf"),"w") as f:
+        mic_data = []
+        vg_labels = []
+        apgd_labels = []
+        T_sti_list = []
+        for clip_name in tqdm(eigenmike_files):
+
+            T_sti = random.choice(T_logs_spaced)
+
+            # print("Clipname ", os.path.basename(clip_name))
+            fs, eigen_sig = wavfile.read(clip_name)
+            vsg_sig, apgd = get_visibility_matrix(eigen_sig, fs, apgd=True, bands=[3], T_sti=T_sti) # visibility graph matrix 32ch 
+
+            mic_sig = eigen_sig[:, [5,9,25,21]] # 4 ch raw MIC
+            mic_vsg_sig, _ = get_visibility_matrix(mic_sig, fs, apgd=False, bands=[3], T_sti=T_sti)
+            mic_data.append(mic_vsg_sig.transpose(1, 0, 2, 3))
+            vg_labels.append(vsg_sig.transpose(1, 0, 2, 3)) # (nframes, nbands, nch, nch)
+            #print(apgd.shape, mic_vsg_sig.shape)
+            apgd_labels.append(apgd.transpose(1, 0, 2))
+            
+            for _ in range(apgd.shape[1]):
+                T_sti_list.append(T_sti)
+
+        a_np = np.vstack(mic_data)
+        b_np = np.vstack(vg_labels)
+        c_np = np.vstack(apgd_labels)
+        d_np = np.vstack(T_sti_list)
+
+        print("shape of a_np", a_np.shape)
+        print("shape of b_np", b_np.shape)
+        print("shape of c_np", c_np.shape)
+        print("shape of d_np", d_np.shape)
+
+        f.create_dataset("mic", shape=a_np.shape, dtype=a_np.dtype, data=a_np)
+        f.create_dataset("em32", shape=b_np.shape, dtype=b_np.dtype, data=b_np)
+        f.create_dataset("apgd", shape=c_np.shape, dtype=c_np.dtype, data=c_np)
+        f.create_dataset("dur", shape=d_np.shape, dtype=d_np.dtype, data=d_np)
+
+        assert(a_np.shape[0] == b_np.shape[0])
+        f.attrs["sr"] = fs
+        
+        del a_np, b_np
+        gc.collect()            
+
+#nbands = 10
+#freq, bw = (skutil  # Center frequencies to form images
+#        .view_as_windows(np.linspace(1500, 4500, nbands), (2,), 1)
+#        .mean(axis=-1)), 50.0  # [Hz]
+#print(freq)
+
+## Parameters used to train network with ARNI+METU dataset that constains some silence
+save_path = "data_hdf"
+os.makedirs(save_path, exist_ok=True)
+data_src = "/scratch/data/repos/LAM/dataset/simulated/output_events_1_maxdur_2s/"
+create_full_hdf_data(dataset_name='output_poly1_log_vardur_log_10msec2sec', data_src=data_src, save_path=save_path)
+#create_full_hdf_data(dataset_name='metu_test9ch_apgd', data_src=data_src, save_path=save_path)
