e2104ffcfd6fdfd0e34c7412cdb42209d7d6c6a6
asroman <asroman@ucdavis.edu>
2024-05-11 23:48:12 -0700

    generate plots with metrics: PSRN, SSIM

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
+from skimage.metrics import structural_similarity as ssim
+
 from trainer.plot_utils import *
 
 from trainer.base_trainer import BaseTrainer
@@ -27,7 +29,7 @@ class Trainer(BaseTrainer):
     def _train_epoch(self, epoch):
         loss_total = 0.0
         
-        for i, (S_in, _) in enumerate(self.train_data_loader):
+        for i, (S_in, _, _) in enumerate(self.train_data_loader):
             S_in = S_in.to(self.device)
             S_in = S_in.unsqueeze(1)
             S_out,_ = self.model(S_in)
@@ -49,8 +51,12 @@ class Trainer(BaseTrainer):
     def _validation_epoch(self, epoch):
         visualize_limit = self.validation_custom_config["visualize_limit"]
         loss_total = 0.0
+        avg_psnr = 0.0
+        psnr_vs_dur = {}
 
-        for i, (S_in, apgd_label) in enumerate(self.validation_data_loader):
+        is_dur_active = False
+
+        for i, (S_in, apgd_label, dur_list) in enumerate(self.validation_data_loader):
             S_in = S_in.to(self.device)
             S_in = S_in.unsqueeze(1)
             S_out, latent_x = self.model(S_in)
@@ -60,7 +66,10 @@ class Trainer(BaseTrainer):
             latent_I /= latent_I.max()
             latent_I = np.tile(latent_I, (3, 1))
             R_field = get_field()
-            ## Generated tesselation for Robinson projection
+            
+            ##############################################################################
+            ############# Generated tesselation for Robinson projection ##################
+            ##############################################################################
             if i <= visualize_limit:
                 fig, axes = plt.subplots(1, 2, figsize=(10, 5)) 
                 ax = axes[0] # subplot 1
@@ -73,8 +82,6 @@ class Trainer(BaseTrainer):
                         fig=fig,
                         ax=ax)
                 
-                ax1.set_title("Latent Acoustic Map {}".format(i))
-
             if i <= visualize_limit:
                 ax = axes[1] # subplot 2
                 arg_lonticks = np.linspace(-180, 180, 5)
@@ -89,10 +96,40 @@ class Trainer(BaseTrainer):
                         fig=fig,
                         ax=ax)
             
-                ax2.set_title("APGD Map {}".format(i))
-
+                
+                gt = apgd_map[0]
+                pred = latent_I[0]
+                ssim_gt = ssim(gt, gt, data_range=gt.max() - gt.min())
+                ssim_pred = ssim(gt, pred, data_range=pred.max() - pred.min())
+                ax1.set_title(f"LAM (pred): SSIM {round(ssim_pred, 2)}") 
+                ax2.set_title(f"APGD (gt): SSIM {round(ssim_gt, 2)}")
                 self.writer.add_figure("Acoustic Map {}".format(i), fig, epoch)
 
+            ##############################################################################
+            ##############################################################################
+
+            # Compute peak SNR between acustic maps (latent_x)
+            psnr_val = psnr(apgd_map[0], latent_I[0])
+            
+            avg_psnr += psnr_val
+
+            # Compute peak SNR as a function of duration
+            if dur_list is not None:
+                for dur in dur_list:
+                    dur_key = dur.item()
+                    if dur_key not in psnr_vs_dur:
+                        psnr_vs_dur[dur_key] = {}
+                        psnr_vs_dur[dur_key]["psnr"] = psnr_val
+                        psnr_vs_dur[dur_key]["count"] = 1
+                    else:
+                        psnr_vs_dur[dur_key]["psnr"] += psnr_val
+                        psnr_vs_dur[dur_key]["count"] += 1
+
+                is_dur_active = True
+
+            ##############################################################################
+            ##################### Visualize visibility matrices ##########################
+            ##############################################################################
             if i <= visualize_limit:
                 fig, ax = plt.subplots(1, 4)
                 mat_out = S_out.detach().cpu().numpy()
@@ -109,9 +146,36 @@ class Trainer(BaseTrainer):
                         ax[j].set_title("Visibility matrix (mag) - label & prediction")
                     ax[j].set_xlabel('Column Index')
                     ax[j].set_ylabel('Row Index')
-                #fig.colorbar(im, ax=ax[1], label='Intensity')
+
                 plt.tight_layout()
                 self.writer.add_figure(f"Visibility matrix - sample {i}", fig, epoch)
+        
+            ##############################################################################
+            ##############################################################################
+
         dl_len = len(self.validation_data_loader)
         self.writer.add_scalar(f"Validation/Loss", loss_total / dl_len, epoch)
+        self.writer.add_scalar(f"Peak SNR", avg_psnr / dl_len, epoch)
+        
+        ###############################################
+        ########### Make batplot for PSNR #############
+        ###############################################
+        if is_dur_active:
+            for dur in psnr_vs_dur:
+                # compute average PSNR per duration                  
+                psnr_vs_dur[dur]["psnr"] /= psnr_vs_dur[dur]["count"]
+            psnr_bins = sorted(list(psnr_vs_dur.keys())) 
+            psnr_vals = [psnr_vs_dur[dur]["psnr"] for dur in psnr_bins]
+            psnr_bins_sec = [round(time_bin * 10, 2) for time_bin in psnr_bins] # convert to seconds scale
+            x = np.arange(len(psnr_bins_sec))
+            fig, ax = plt.subplots(1, 1)       
+            ax.set_ylabel('PSNR')
+            ax.set_xlabel('Audio frame duration (s)')
+            ax.set_title('PSRN vs. frame duration')
+
+            ax.bar(x, psnr_vals, width = 0.4)
+            ax.set_xticks(x)
+            ax.set_xticklabels(psnr_bins_sec)
+            
+            self.writer.add_figure(f"Dur vs PSNR/Barplot", fig, epoch)
         return 0
