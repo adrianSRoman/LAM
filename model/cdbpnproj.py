dff9a4b3b6c6086ba7af3e2aed2b11b1243afe86
asroman <asroman@ucdavis.edu>
2024-05-23 11:51:38 -0700

    add cdbpn->bproj model

--- model/cdbpnproj.py
+++ model/cdbpnproj.py
@@ -9,9 +9,10 @@ from cdbpn import Net as CDBPN
 class CDBPNProj(nn.Module):
     def __init__(self, num_channels=9, base_filter=32, feat = 128, num_stages=10, scale_factor=8):
         super(CDBPNProj, self).__init__()
-
         self.cdbpn = CDBPN(num_channels, base_filter,  feat, num_stages, scale_factor=upscale_factor)
         self.bproj = BackProjLayer()
 
     def forward(self, S):
-        pass
+        S_pred = self.cdbpn(S)      # perform upsampling
+        out, x = self.bproj(S_pred) # get prediction and latent
+        return out, x
