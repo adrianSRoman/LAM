901ebf5368391275e1252aea398355acf432d0cd
asroman <asroman@ucdavis.edu>
2024-06-19 22:59:52 -0700

    add eigenvalue dispersion loss

--- model/loss.py
+++ model/loss.py
@@ -12,8 +12,7 @@ class l1_reg_loss(nn.Module):
     def __init__(self):
         super(l1_reg_loss, self).__init__()
         self.R_xyz = torch.from_numpy(get_field()).to('cuda:0')
-        print("initialized R", self.R_xyz.shape, self.R_xyz[0].shape)
-        self.N_max = 12
+        self.N_max = 20
         self.l1_loss = torch.nn.L1Loss()
         
     def forward(self, target, pred, latent):
@@ -24,7 +23,31 @@ class l1_reg_loss(nn.Module):
         # get the coordinates of the N_max values only
         max_xyz = self.R_xyz[:, max_idx].T
         # parwise ditances
-        avg_dist = torch.nn.functional.pdist(max_xyz, p=2)
-        avg_dist = torch.sum(avg_dist)
-        total_loss = l1_loss + avg_dist/self.N_max
-        return total_loss
+        dists = torch.nn.functional.pdist(max_xyz, p=2)
+        std_dist = torch.std(dists)
+        total_loss = l1_loss + std_dist
+        return total_loss, std_dist
+
+class l1_cov_loss(nn.Module):
+    def __init__(self, N_max=20):
+        super(l1_cov_loss, self).__init__()
+        self.R_xyz = torch.from_numpy(get_field()).to('cuda:0')
+        self.N_max = N_max
+        print("Max value being loaded", N_max)
+        self.l1_loss = torch.nn.L1Loss()
+        
+    def forward(self, target, pred, latent):
+        l1_loss = self.l1_loss(target, pred)
+        # Sort the latent tensor and get indices of top N_max values
+        _, sorted_indices = torch.sort(latent, descending=True)
+        max_idx = sorted_indices[:self.N_max]
+        # get the coordinates of the N_max values only
+        max_xyz = self.R_xyz[:, max_idx].T
+        # get mean coordinate
+        mean_xyz = max_xyz.mean(dim=0)
+        centered_xyz = max_xyz - mean_xyz
+        cov_matrix = torch.matmul(centered_xyz.t(), centered_xyz) / (max_xyz.size(0) - 1)
+        e_vals, e_vecs = torch.linalg.eigh(cov_matrix)
+        e_val_sum = torch.sum(e_vals)
+        total_loss = l1_loss + 0.02*e_val_sum
+        return total_loss, l1_loss, 0.02*e_val_sum
