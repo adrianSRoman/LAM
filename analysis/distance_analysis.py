73a06ebbb0699efbd01e43a50287f4320e87b570
asroman <asroman@ucdavis.edu>
2024-07-16 21:58:27 -0700

    put simulation in loop through frequency bands

--- analysis/distance_analysis.py
+++ analysis/distance_analysis.py
@@ -6,7 +6,7 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 
-from trainer.utils import draw_map, get_field
+from trainer.utils import draw_map, get_field, rms_normalizer
 from dataset.gen_dataset.gen_dataset import get_visibility_matrix
 from dataset.simulated.utils import get_arni_dataset
 
@@ -16,8 +16,8 @@ aud_fmt = "em32" # audio format: em32, mic
 FS = 24000 # sampling rate
 duration = 2 # sythesis duration
 num_chans = 32 # get number of channels from RIR
-img_indx = 10 # time index image to study (e.g. 10 == 10th ms)
-freq_indx = 8 # frequency index to study
+img_indx = 0 # time index image to study (e.g. 10 == 10th ms)
+freq_indx = 5 # frequency index to study
 bands = np.arange(10)
 
 audio_clips = [
@@ -33,78 +33,94 @@ audio_clips = [
     "/scratch/data/repos/SpatialScaper/datasets/sound_event_datasets/FSD50K_FMA/music/test/Rock/000255.mp3"
 ]
 
-conv_signals = []
-var_dist_list = [] # variance distance list
-max_dist_list = [] # maximum pixel per distance in list
-for rir_tokens in rirs_list:
-    clips_var_list = []
-    max_azimuth = [] # list of azimuth values of the highest intensity pixel
-    for clip in audio_clips:
-        output_sig = np.zeros((FS * duration, num_chans)) # initialize empty output convolved signal
-        rir_name = rir_tokens[0] + rir_tokens[1] + rir_tokens[2] # get tokens to RIR names
-        ir_path = os.path.join(METU_DATASET, rir_name, f"IR_{aud_fmt}.wav")
-        irdata, sr = librosa.load(ir_path, mono=False, sr=FS)
-
-        rir_sig = irdata.T        
-
-        aud_sig, sr = librosa.load(clip, mono=True, sr=None)
-        if sr != FS:
-            aud_sig = librosa.resample(aud_sig, orig_sr=sr, target_sr=FS)
-
-        # Perform signal convolution - spatialize it
-        for ichan in range(num_chans):
-            output_sig[:, ichan] += np.convolve(aud_sig[:FS * duration], rir_sig[:FS, ichan], mode='same')
-
-        # get acoustic map
-        T_sti = 10e-3 # 100ms audio frames
-        vsg_sig, apgd = get_visibility_matrix(output_sig, FS, apgd=True, bands=bands, T_sti=T_sti) # visibility graph matrix 32ch
-        apgd_map = np.abs(apgd[freq_indx, img_indx]) # apdg original shape (nbands, ntime, npixels)
+def distance_simulation(freq_indx, use_noise=False):
+    conv_signals = []
+    var_dist_list = [] # variance distance list
+    max_dist_list = [] # maximum pixel per distance in list
+    for rir_tokens in rirs_list:
+        clips_var_list = []
+        max_azimuth = [] # list of azimuth values of the highest intensity pixel
+        for clip in audio_clips:
+            output_sig = np.zeros((FS * duration, num_chans)) # initialize empty output convolved signal
+            rir_name = rir_tokens[0] + rir_tokens[1] + rir_tokens[2] # get tokens to RIR names
+            ir_path = os.path.join(METU_DATASET, rir_name, f"IR_{aud_fmt}.wav")
+            irdata, sr = librosa.load(ir_path, mono=False, sr=FS)
+            if sr != FS:
+                irdata = librosa.resample(irdata, orig_sr=sr, target_sr=FS, axis=-1)
+            irdata *= 0.3 # Normalize to ~30dBFS
         
-        # Get the indices of the top 30 maximum values in apgd_map
-        max_indices = np.argpartition(apgd_map, -30)[-30:]
-        max_indices = max_indices[np.argsort(-apgd_map[max_indices])]
+            irdata = rms_normalizer(irdata) # appply normalization
+            rir_sig = irdata.T        
+
+            aud_sig, sr = librosa.load(clip, mono=True, sr=None)
+            if sr != FS:
+                aud_sig = librosa.resample(aud_sig, orig_sr=sr, target_sr=FS)
+            aud_sig = rms_normalizer(aud_sig)
+    
+            if use_noise: # overwrite signal with gaussian noise (whitenoise)
+                aud_sig = torch.randn(duration*FS)
+
+            # Perform signal convolution - spatialize it
+            for ichan in range(num_chans):
+                output_sig[:, ichan] += np.convolve(aud_sig[:FS * duration], rir_sig[:FS, ichan], mode='same')
+
+            # get acoustic map
+            T_sti = 10e-3 * 20 # 100ms audio frames
+            vsg_sig, apgd = get_visibility_matrix(output_sig, FS, apgd=True, bands=bands, T_sti=T_sti) # visibility graph matrix 32ch
+            apgd_map = np.abs(apgd[freq_indx, img_indx]) # apdg original shape (nbands, ntime, npixels)
         
-        # get acoustic field and shift to azimuth zero if needed
-        R_xyz = get_field(shift_lon=rirs_list[rir_tokens])
+            # Get the indices of the top 30 maximum values in apgd_map
+            max_indices = np.argpartition(apgd_map, -28)[-28:]
+            max_indices = max_indices[np.argsort(-apgd_map[max_indices])]
+        
+            # get acoustic field and shift to azimuth zero if needed
+            R_xyz = get_field(shift_lon=rirs_list[rir_tokens])
 
-        # Get the corresponding coordinates for the top indices
-        max_xyz = R_xyz[:, max_indices].T
-        max_azimuth.append(max_xyz[0]) # add max element coordinate to the list
-        # Calculate the pairwise distances
-        dists = np.sqrt(np.sum((max_xyz[0, np.newaxis] - max_xyz[np.newaxis, :])**2, axis=2))
-        dists = dists[np.triu_indices_from(dists, k=1)]
+            # Get the corresponding coordinates for the top indices
+            max_xyz = R_xyz[:, max_indices].T
+            max_azimuth.append(max_xyz[0]) # add max element coordinate to the list
+            # Calculate the pairwise distances
+            dists = np.sqrt(np.sum((max_xyz[0, np.newaxis] - max_xyz[np.newaxis, :])**2, axis=2))
+            dists = dists[np.triu_indices_from(dists, k=1)]
 
-        # Calculate the standard deviation of the distances
-        std_dist = np.std(dists)
-        clips_var_list.append(std_dist**2) # add variance to the list
+            # Calculate the standard deviation of the distances
+            std_dist = np.std(dists)
+            clips_var_list.append(std_dist**2) # add variance to the list
 
-    var_dist_list.append(clips_var_list) # add list of variances per clips list
-    max_dist_list.append(max_azimuth)
+        var_dist_list.append(clips_var_list) # add list of variances per clips list
+        max_dist_list.append(max_azimuth)
+
+    return conv_signals, var_dist_list, max_dist_list
+
 
 ################################################################################
 ############# Generate plot from the collected data above ######################
 ################################################################################
-gains_labels = [2, 2.8284271247461903, 3, 4.242640687119286]
-mean_variances = [np.mean(np.array(variances)) for variances in var_dist_list]
-std_variances = [np.std(np.array(variances)) for variances in var_dist_list]
+for freq_index in [0, 1, 2, 3, 4, 5, 6, 7, 8]:
+    conv_signals, var_dist_list, max_dist_list = distance_simulation(freq_index, False)
 
-# calculate interval manually using the formula
-y_est = np.array(mean_variances)
-y_err = np.array(std_variances)
+    gains_labels = [2, 2.8284271247461903, 3, 4.242640687119286]
+    mean_variances = [np.mean(np.array(variances)) for variances in var_dist_list]
+    std_variances = [np.std(np.array(variances)) for variances in var_dist_list]
 
-fig, ax = plt.subplots()
+    # calculate interval manually using the formula
+    y_est = np.array(mean_variances)
+    y_err = np.array(std_variances)
 
-# plot manually calculated interval (std interval) --- the blue one
-ax.plot(np.arange(len(gains_labels)), y_est, '-')
-ax.fill_between(np.arange(len(gains_labels)), y_est - y_err, y_est + y_err, alpha=0.2)
+    fig, ax = plt.subplots()
 
-# Set x-ticks and x-tick labels
-ax.set_xticks(np.arange(len(gains_labels)))
-ax.set_xticklabels(gains_labels)
+    # plot manually calculated interval (std interval) --- the blue one
+    ax.plot(np.arange(len(gains_labels)), y_est, '-')
+    ax.fill_between(np.arange(len(gains_labels)), y_est - y_err, y_est + y_err, alpha=0.2)
 
-# Set x-axis and y-axis labels
-ax.set_title('Mean variance across 10 music tracks w.r.t distance')
-ax.set_xlabel('Distance (m)')
-ax.set_ylabel('DoA Intensity Variance (dispersion)')
+    # Set x-ticks and x-tick labels
+    ax.set_xticks(np.arange(len(gains_labels)))
+    ax.set_xticklabels(gains_labels)
 
-fig.savefig("./figures/distance_analysis.png")
+    # Set x-axis and y-axis labels
+    ax.set_title(f'Mean variance across 10 music tracks w.r.t distance \n freq band {freq_index}')
+    #ax.set_title(f'Mean variance across of whitenoise source w.r.t distance \n freq band {freq_index}')
+    ax.set_xlabel('Distance (m)')
+    ax.set_ylabel('DoA Intensity Variance (dispersion)')
+
+    fig.savefig(f"./figures/distance_simulation_freq{freq_index}_music.png")
