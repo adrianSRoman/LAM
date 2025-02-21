754c790cc2b0fd2dfc00949496e96b7f66d22cca
asroman <asroman@ucdavis.edu>
2025-02-20 23:02:39 -0800

    rename bproj to be a simple LAM layer

--- model/LAM_layer.py
+++ model/LAM_layer.py
@@ -6,18 +6,6 @@ import torch.nn.functional as F
 import math
 from trainer.utils import steering_operator
 
-class ReTanh(torch.nn.Module):
-    '''
-    Rectified Hyperbolic Tangent
-    '''
-    def __init__(self, alpha=1.0):
-        super().__init__()
-        self.alpha = alpha
-
-    def forward(self, x):
-        beta = self.alpha / torch.tanh(torch.tensor(1.0, dtype=x.dtype))
-        return torch.max(torch.tensor(1e-9, dtype=x.dtype), beta * torch.tanh(x))
-
 
 def initialize_scaled_kaiming(layer, scale=1e-6):
     torch.nn.init.kaiming_uniform_(layer.weight, a=0, mode='fan_in', nonlinearity='relu')
@@ -35,30 +23,27 @@ class BackProjLayer(torch.nn.Module):
         if tau is None or D is None:
             self.tau = torch.nn.Parameter(torch.empty((Npx), dtype=torch.float64))
             self.D = torch.nn.Parameter(torch.empty((Nch, Npx), dtype=torch.complex128))
-            self.AA = torch.nn.Parameter(torch.empty(self.A.shape, dtype=torch.complex128))
             self.reset_parameters()
         else:
             self.tau = torch.nn.Parameter(tau)
             self.D = torch.nn.Parameter(D)
-            self.AA = torch.nn.Parameter(torch.empty(self.A.shape, dtype=torch.complex128))
-        self.retanh = nn.ReLU() #ReTanh(alpha=1.000)
+        self.relu = nn.ReLU()
 
         # layers used for cascaded conv with small bias
-        self.smooth1 = torch.nn.Conv1d(1, 1, kernel_size=3, padding=1, dtype=torch.float64)
-        self.smooth2 = torch.nn.Conv1d(1, 1, kernel_size=5, padding=2, dtype=torch.float64)
-        self.smooth3 = torch.nn.Conv1d(1, 1, kernel_size=7, padding=3, dtype=torch.float64)
-        self.smooth4 = torch.nn.Conv1d(1, 1, kernel_size=9, padding=4, dtype=torch.float64)
-        # he initializtion with small variance to handle small numbers better
-        initialize_scaled_kaiming(self.smooth1)
-        initialize_scaled_kaiming(self.smooth2)
-        initialize_scaled_kaiming(self.smooth3)
-        initialize_scaled_kaiming(self.smooth4)
+        self.denoise1 = torch.nn.Conv1d(1, 1, kernel_size=3, padding=1, dtype=torch.float64)
+        self.denoise2 = torch.nn.Conv1d(1, 1, kernel_size=5, padding=2, dtype=torch.float64)
+        self.denoise3 = torch.nn.Conv1d(1, 1, kernel_size=7, padding=3, dtype=torch.float64)
+        self.denoise4 = torch.nn.Conv1d(1, 1, kernel_size=9, padding=4, dtype=torch.float64)
+        # He initializtion with small variance to handle small numbers better
+        initialize_scaled_kaiming(self.denoise1)
+        initialize_scaled_kaiming(self.denoise2)
+        initialize_scaled_kaiming(self.denoise3)
+        initialize_scaled_kaiming(self.denoise4)
 
     def reset_parameters(self):
         std = 1e-5 # changes this from 1e-4
         self.tau.data.normal_(0, 1e-7)
         self.D.data.normal_(0, std)
-        self.AA.data = self.A + (torch.randn_like(self.A) * std).to(self.A.dtype)
 
     def forward(self, S):
         device = S.device
@@ -74,16 +59,16 @@ class BackProjLayer(torch.nn.Module):
         latent_x -= self.tau
         latent_x = latent_x.unsqueeze(1)
         latent_x_skip = latent_x
-        latent_s1 = self.smooth1(latent_x)
+        latent_s1 = self.denoise1(latent_x)
         latent_x  = latent_x_skip.add(latent_s1)
         latent_x  = self.relu(latent_x)
-        latent_s2 = self.smooth2(latent_x)
+        latent_s2 = self.denoise2(latent_x)
         latent_x  = latent_x_skip.add(latent_s2)
         latent_x  = self.relu(latent_x)
-        latent_s3 = self.smooth3(latent_x)
+        latent_s3 = self.denoise3(latent_x)
         latent_x  = latent_x_skip.add(latent_s3)
         latent_x  = self.relu(latent_x)
-        latent_s4 = self.smooth4(latent_x)
+        latent_s4 = self.denoise4(latent_x)
         latent_x  = latent_x_skip.add(latent_s4)
         latent_x  = self.relu(latent_x)
         latent_x  = latent_x.squeeze(1)
